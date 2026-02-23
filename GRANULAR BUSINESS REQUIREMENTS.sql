dd02238c7de8d9c56e43df261bcc56831db19f24
dhananjaykillewale@gmail.com <98831202+DhananjayKillewale@users.noreply.github.com>
2026-02-24 03:22:44 +0530

    Updated Business requirements.

--- GRANULAR BUSINESS REQUIREMENTS.sql
+++ GRANULAR BUSINESS REQUIREMENTS.sql
@@ -1,6 +1,7 @@
 --GRANULAR BUSINESS REQUIREMENTS 
 
 --1. Total Sales by fat Content:
+
 select Item_Fat_Content, 
 cast(sum(Total_Sales)/1000 as decimal(10,2)) as Total_Sales_Thousands, 
 cast(avg(Total_Sales) as decimal(10,0)) as Avg_Sales, 
@@ -11,6 +12,7 @@ group by Item_Fat_Content
 order by Total_Sales_Thousands DESC
 
 --2. Total Sales by Item type:
+
 select Item_Type, 
 cast(sum(Total_Sales)/1000 as decimal(10,2)) as Total_Sales, 
 cast(avg(Total_Sales) as decimal(10,0)) as Avg_Sales, 
@@ -42,16 +44,45 @@ order by Outlet_Location_Type DESC
 --4. Total Sales by Outlet Establishment
 
 select Outlet_Establishment_Year, 
-cast(sum(Total_Sales)/1000 as decimal(10,2)) as Total_Sales
+cast(sum(Total_Sales) as decimal(10,2)) as Total_Sales,
+cast(avg(Total_Sales) as decimal(10,1)) as Avg_Sales, 
+count(*) as No_Of_Items,
+cast(avg(Rating) as decimal(10,2)) as Avg_Rating
 from [BlinkIT Grocery Data]
 group by Outlet_Establishment_Year
-order by Outlet_Establishment_Year ASC
+order by Total_Sales DESC 
 
 --5. Percentage of Sales by Outlet size
 
-select Outlet_Size,
+select 
+Outlet_Size,
 cast(sum(Total_Sales) as decimal(10,2)) as Total_Sales,
-cast(sum(Total_Sales) * 100.0/sum(sum(Total_Sales) over()) as decimal(10,2)) as Sales_Percentage
+cast((sum(Total_Sales) * 100.0/sum(sum(Total_Sales)) over()) as decimal(10,2)) AS Sales_Percentage
 from [BlinkIT Grocery Data]
 group by Outlet_Size
 order by Total_Sales DESC;
+
+--6. Sales by Outlet location:
+ 
+select Outlet_Location_Type, 
+cast(sum(Total_Sales) as decimal(10,2)) as Total_Sales,
+cast((sum(Total_Sales) * 100.0/sum(sum(Total_Sales)) over()) as decimal(10,2)) AS Sales_Percentage,
+cast(avg(Total_Sales) as decimal(10,1)) as Avg_Sales, 
+count(*) as No_Of_Items,
+cast(avg(Rating) as decimal(10,2)) as Avg_Rating
+from [BlinkIT Grocery Data]
+group by Outlet_Location_Type  
+order by Total_Sales DESC 
+
+
+--7. All metric by Outlet Type:
+
+select Outlet_Type, 
+cast(sum(Total_Sales) as decimal(10,2)) as Total_Sales,
+cast((sum(Total_Sales) * 100.0/sum(sum(Total_Sales)) over()) as decimal(10,2)) AS Sales_Percentage,
+cast(avg(Total_Sales) as decimal(10,1)) as Avg_Sales, 
+count(*) as No_Of_Items,
+cast(avg(Rating) as decimal(10,2)) as Avg_Rating
+from [BlinkIT Grocery Data]
+group by Outlet_Type  
+order by Total_Sales DESC 
